1bf8e081a5db425e418b43ab09d4196c2a5d4884
Luffy <singhrajvardhan69@gmail.com>
2023-07-31 17:45:51 +0530

    Template

---
+++ Templates/Luffy.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define eb emplace_back
+#define m_p make_pair 
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define maxi(v) *max_element((v).begin(),(v).end())
+#define mini(v) *min_element((v).begin(),(v).end())
+#define sum(v) accumulate((v).begin(),(v).end(),0ll)
+#define rev(v) reverse((v).begin(),(v).end())
+#define all(v) (v).begin(),(v).end()
+#define ed '\n'
+#define Ed '\n'
+#define no cout<<"NO"<<ed
+#define yes cout<<"YES"<<ed
+int max(int a,int b) {return (a>b ? a:b);}
+int min(int a,int b) {return (a<b ? a:b);}
+int gcd(int a,int b) {return (b == 0 ? a : gcd(b, a % b));}
+int lcm(int a,int b) {return (a / gcd(a, b)) * b;}
+typedef vector<int>     vi;   typedef vector<vector<int>> vvi;
+void solve(){
+	
+}
+signed  main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int _t=1;
+    cin>>_t;
+    while(_t--) solve();
+}
